dfb97225e9d473c91228cd84680fae17299edcbe
Requena <>
2013-02-10 12:55:15 +0100

    just justify code

--- recipes/restore.rb
+++ recipes/restore.rb
@@ -11,8 +11,8 @@ script "restore_mediawiki_by_sql" do
   cwd "/tmp"
   only_if do File.exists?(node[:mediawiki][:directory]+"/backup.sql") end
   code <<-EOH
-  mysql -u #{node[:mediawiki][:dbuser]} -p#{node[:mediawiki][:dbpass]} #{node[:mediawiki][:dbname]} < #{node[:mediawiki][:directory]}/backup.sql
-  rm -f  #{node[:mediawiki][:directory]}/backup.sql
+    mysql -u #{node[:mediawiki][:dbuser]} -p#{node[:mediawiki][:dbpass]} #{node[:mediawiki][:dbname]} < #{node[:mediawiki][:directory]}/backup.sql
+    rm -f  #{node[:mediawiki][:directory]}/backup.sql
   EOH
 end
 
@@ -22,15 +22,15 @@ script "restore_mediawiki_by_tar_gz" do
   cwd node[:mediawiki][:directory]
   only_if do File.exists?(node[:mediawiki][:directory]+"/backup.tar.gz") end
   code <<-EOH
-  cd #{node[:mediawiki][:directory]}
-  tar xvzf backup.tar.gz
-  chown -R apache:apache images
-  chown -R apache:apache skins 
-  chown -R apache:apache extensions
-  php maintenance/importDump.php backup.xml
-  php maintenance/rebuildrecentchanges.php
-  rm -rf backup.xml
-  rm -rf backup.tar.gz
+    cd #{node[:mediawiki][:directory]}
+    tar xvzf backup.tar.gz
+    chown -R apache:apache images
+    chown -R apache:apache skins
+    chown -R apache:apache extensions
+    php maintenance/importDump.php backup.xml
+    php maintenance/rebuildrecentchanges.php
+    rm -rf backup.xml
+    rm -rf backup.tar.gz
   EOH
 end
 
